7731f3a0edfe639dcb1cbd3882b082ade773ac29
Saular Raffi <fo6197@wayne.edu>
2020-03-06 13:21:40 -0500

    null

--- TicTacTracers/app/src/main/java/com/example/tictactracers/MainActivity.java
+++ TicTacTracers/app/src/main/java/com/example/tictactracers/MainActivity.java
@@ -4,8 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+//    ImageButton button_00 = findViewById(R.id.button_00);
+//    ImageButton button_01 = findViewById(R.id.button_01);
+//    ImageButton button_02 = findViewById(R.id.button_02);
+//    ImageButton button_10 = findViewById(R.id.button_10);
+//    ImageButton button_11 = findViewById(R.id.button_11);
+//    ImageButton button_12 = findViewById(R.id.button_12);
+//    ImageButton button_20 = findViewById(R.id.button_20);
+//    ImageButton button_21 = findViewById(R.id.button_21);
+//    ImageButton button_22 = findViewById(R.id.button_22);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+//        Log.i("Log", "This is a test");
     }
 }
